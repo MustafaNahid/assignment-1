8624d4a305ab02dead6c2e7f9deea3d25df7ab4a
MustafaNahid <nahid.mustafa@gmail.com>
2024-07-08 21:51:50 +0600

    Add files via upload

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,7 @@
+<?php
+
+$tempCelsius = 32;
+
+$tempFahrenheit = ($tempCelsius * 9/5) + 32;
+
+printf("The conversion of temperature of %d degrees Celsius is equal to %.2f degrees Fahrenheit.", $tempCelsius, $tempFahrenheit);
